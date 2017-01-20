1645be183168dc859de212ad77152e34b8038e5c
Pavel <pavel.svarc@cpodesign.com>
2017-01-20 07:21:15 +0000

    added repository base class
    Example of base repository class using dapper

---
+++ Runner/RepositoryBase.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using FizzWare.NBuilder;
+
+namespace Runner
+{
+public abstract class DapperExRepositoryBase<TEntity> where TEntity : class
+{
+    public virtual bool Delete(TEntity entity)
+    {
+        using (var conn = ConnectionFactory.GetConnection())
+        {
+            return conn.Delete(entity);
+        }
+    }
+ 
+    public virtual dynamic Insert(TEntity entity)
+    {
+        using (var conn = ConnectionFactory.GetConnection())
+        {
+            return conn.Insert(entity);
+        }
+    }
+ 
+    public virtual bool Update(TEntity entity)
+    {
+        using (var conn = ConnectionFactory.GetConnection())
+        {
+            return conn.Update(entity);
+        }
+    }
+}
+}
